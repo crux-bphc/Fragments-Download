06d48b19acd824eb2f7397709e8c5bde87e988b5
him1411 <himjan1416@gmail.com>
2017-08-05 16:59:39 +0530

    removing the previous files

--- progressBar.py
+++
@@ -1,8 +0,0 @@
-def printProgress(percent,width=100):
-    chars=percent*width/100;
-    print('[',end='')
-    print('N'*int(chars),end='');
-    print('-'*(width-int(chars)),end='')
-    print(']',end='')
-    print(" "+str(percent)+"%",end='')
-    print('\r',end='')
